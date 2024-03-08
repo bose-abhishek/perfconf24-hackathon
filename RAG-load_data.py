d304811aa35042398ab25f324bb6722e5d966234
Abhishek Bose <abose@redhat.com>
2024-03-08 10:21:08 +0530

    Added HFE for data query and made some minor modifications

--- RAG-load_data.py
+++ RAG-load_data.py
@@ -2,41 +2,46 @@ import requests
 import json
 import PyPDF2
 import urllib3
-#from chromadb import chromadb
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain_community.document_loaders import DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.schema import Document
 from langchain_community.vectorstores.chroma import Chroma
 from chromadb.utils import embedding_functions
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 import os
 import shutil
+import sys
+from datetime import datetime
 
 # Disable SSL verification
 urllib3.disable_warnings()
 
 CHROMA_PATH = "chroma"
-DATA_PATH = "/home/test/salesforce_report1.csv"
+DATA_PATH = "/home/test/Downloads/report1709795410593.csv"
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
 
-#load doc from single PDF
+print("Current Time =", current_time)
+#load doc from single PDFpython
 #loader = PyPDFLoader(DATA_PATH)
 #docs = loader.load()
-#text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
 
 #load from csv file
 loader = CSVLoader(DATA_PATH)
 docs = loader.load()
+
+print(current_time, " doc loading completed")
+
 text_splitter = RecursiveCharacterTextSplitter(separators = "\n", chunk_size=500, chunk_overlap=100)
-
 chunks = text_splitter.split_documents(docs)
-print(len(chunks))
+print("Number of chunks:", len(chunks))
 
+print(current_time, " Transformer Embedding begins")
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+print(current_time, " Transformer Embedding Ends")
 
 if os.path.exists(CHROMA_PATH):
     shutil.rmtree(CHROMA_PATH)
-
+print(current_time, " Adding vectors to DB begins")
 db = Chroma.from_documents(documents=chunks, embedding=embedding_function, persist_directory=CHROMA_PATH, collection_metadata={"hnsw:space": "cosine"})
 db.persist()
+print(current_time, " Adding vectors to DB Ends")
--- RAG-query.py
+++ RAG-query.py
@@ -1,16 +1,22 @@
 import requests
 import json
-import argparse
 import urllib3
 import os
 from langchain_community.vectorstores.chroma import Chroma
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.prompts import ChatPromptTemplate
+#from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_community.llms import HuggingFaceEndpoint
+#from langchain.chains.question_answering import load_qa_chain
 
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 # Disable SSL verification
 urllib3.disable_warnings()
 
 CHROMA_PATH = "chroma"
+URL="https://llama-2-7b-chat-perfconf-hackathon.apps.dripberg-dgx2.rdu3.labs.perfscale.redhat.com"
+endpoint="/generate"
 
 def format_llama_prompt(user_prompt):
     prompt = """\
@@ -38,7 +44,7 @@ embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"
 
 db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-#query = "what is PG autoscaling?"
+#query = "list all Ansible performance related cases"
 query = input("Enter your query to search: ")
 
 results = db.similarity_search_with_relevance_scores(query, k=15)
@@ -47,27 +53,24 @@ results = db.similarity_search_with_relevance_scores(query, k=15)
 context_text = "\n---\n".join([doc.page_content for doc, _score in results])
 prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
 prompt_RAG = prompt_template.format(context=context_text, question=query)
-#print(prompt_RAG)
-
-URL="https://llama-2-7b-chat-perfconf-hackathon.apps.dripberg-dgx2.rdu3.labs.perfscale.redhat.com"
-
-endpoint="/generate"
-
-headers = {
-        "Content-Type": "application/json"
-        }
-
 prompt = format_llama_prompt(prompt_RAG)
 
-data = {
-    "inputs": prompt,
-    "parameters": {
-        "max_new_tokens": 600,
-        "temperature": 0.9, # Just an example
-        "repetition_penalty": 1.03, # Just an example
-        "details": False
-        }
-}
 
-response = requests.post(f"{URL}{endpoint}", headers=headers, json=data, verify=False) #, stream=True)
-print(response.json().get("generated_text"))
+llm = HuggingFaceEndpoint(
+    endpoint_url=f"{URL}",
+    max_new_tokens=512,
+    top_k=10,
+    top_p=0.95,
+    typical_p=0.95,
+    temperature=0.9,
+    repetition_penalty=1.03,
+)
+
+#llm(query)
+
+response = llm.predict(prompt)
+print(response)
+
+#chain = load_qa_chain(llm,chain_type="stuff")
+#response = chain.run(input_documents=results, question=query)
+#print(response)
