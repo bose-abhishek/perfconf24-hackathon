c79b3d4df1ffaec3465d86e75c814f5d2842b6de
jainnikhil30 <jainnikhil30@gmail.com>
2024-03-13 16:20:16 +0530

    add the UI

--- queryLLM.py
+++ queryLLM.py
@@ -3,6 +3,7 @@ from langchain_community.vectorstores import Pinecone as PC
 from langchain_community.llms import HuggingFaceEndpoint
 from langchain.prompts import ChatPromptTemplate
 from configparser import ConfigParser
+import streamlit as st
 import os
 
 import urllib3
@@ -43,30 +44,36 @@ def main():
     PINECONE_API_KEY = settings.get('settings', 'PINECONE_API_KEY')
     index_name = settings.get('settings', 'index_name')
     INFERENCE_URL = settings.get('settings', 'INFERENCE_URL')
-    query = input("Enter your query to search: ")
-    os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
-    os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
+    left_co, cent_co,last_co = st.columns(3)
+    with cent_co:
+        st.image('llama_drama.png')
+    query = st.text_input("prompt", value="", key="prompt")
+    if query!= "":
+        #query = input("Enter your query to search: ")
+        os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
+        os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
 
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
-    docsearch=PC.from_existing_index(index_name, embeddings)
-    docs=docsearch.similarity_search(query, k=10)
+        embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+        docsearch=PC.from_existing_index(index_name, embeddings)
+        docs=docsearch.similarity_search(query, k=10)
 
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt_RAG = prompt_template.format(context=docs, question=query)
+        prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+        prompt_RAG = prompt_template.format(context=docs, question=query)
 
-    prompt = format_llama_prompt(prompt_RAG)
+        prompt = format_llama_prompt(prompt_RAG)
 
-    llm = HuggingFaceEndpoint(
-        endpoint_url=f"{INFERENCE_URL}",
-        max_new_tokens=512,
-        top_k=10,
-        top_p=0.95,
-        typical_p=0.95,
-        temperature=0.9,
-        repetition_penalty=1.03,
-    )
-    response = llm.invoke(prompt)
-    print(response)
+        llm = HuggingFaceEndpoint(
+            endpoint_url=f"{INFERENCE_URL}",
+            max_new_tokens=512,
+            top_k=10,
+            top_p=0.95,
+            typical_p=0.95,
+            temperature=0.9,
+            repetition_penalty=1.03,
+        )
+        response = llm.invoke(prompt)
+        #print(response)
+        st.markdown(response)
 
 
 if __name__ == "__main__":
